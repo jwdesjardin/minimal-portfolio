e2103c9f1d957d343d87fb77125012530c18807c
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 11:03:11 -0700

    add interested component

---
+++ project/components/Interested/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Interested = () => {
+	return (
+		<div className='flex flex-col items-center justify-center'>
+			<h2 className='text-4xl font-serif font-bold px-12 text-center'>
+				Interested in doing a project together?
+			</h2>
+			<button className='border-myGrey-900 px-6 py-3 my-8 border-2 uppercase'>Contact Me</button>
+		</div>
+	)
+}
