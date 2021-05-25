8db2e70dcd841b98ee15de50172f4bfc0ae0ca48
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 11:05:19 -0700

    upadte tailwin config

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,12 +1,40 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: 'jit',
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				myCyan: {
+					500: '#5FB4A2',
+				},
+				myBlue: {
+					700: '#203A4C',
+				},
+				myGrey: {
+					100: '#EAEAEB',
+					300: '#D7D7D7',
+					900: '#33323D',
+				},
+				myRed: {
+					600: '#F43030',
+				},
+			},
+			fontFamily: {
+				sans: ['Public Sans', 'sans-serif'],
+				serif: ['Ibarra Real Nova', 'serif'],
+			},
+			fontSize: {
+				xs: '15px',
+				sm: '16px',
+				md: '32px',
+				lg: '40px',
+				xl: '50px',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
