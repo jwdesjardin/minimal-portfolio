297fa73a5ce3a9374b4c580ed9f548c4135b2355
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 15:51:00 -0700

    build Project Hero component with tailwind

---
+++ project/components/ProjectHero/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ProjectHero = () => {
+	return (
+		<section className='content-container pt-0 md:pt-24 pb-10 xl:pb-24 '>
+			<img src='/images/detail/desktop/image-bookmark-hero.jpg' alt='' />
+		</section>
+	)
+}
