df8955578fbfeec4a55b822b76fe2727fe4e3d3c
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 15:52:17 -0700

    compile components to build a sample project page --mising navigation

---
+++ project/pages/project.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+
+// ProjectHero
+import { ProjectHero } from '../components/ProjectHero'
+import { ProjectDetails } from '../components/ProjectDetails'
+import { ProjectContent } from '../components/ProjectContent'
+// ProjectDetails
+// ProjectContent
+// Navigation
+
+import { Interested } from '../components/Interested'
+import { Footer } from '../components/Footer'
+
+export default function Home() {
+	return (
+		<div className=''>
+			<Head>
+				<title>Projects</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main className='w-full'>
+				<Navbar></Navbar>
+				<ProjectHero></ProjectHero>
+				<section className='content-container xl:flex xl:items-start'>
+					<ProjectDetails></ProjectDetails>
+					<ProjectContent></ProjectContent>
+				</section>
+
+				<Interested></Interested>
+				<Footer></Footer>
+			</main>
+		</div>
+	)
+}
