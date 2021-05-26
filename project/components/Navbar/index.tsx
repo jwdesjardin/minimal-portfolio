aa0dda2f7d30b1434db7ab222d51e44df1a792a9
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 22:00:04 -0700

    adds dropdown menu to navbar

--- project/components/Navbar/index.tsx
+++ project/components/Navbar/index.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HamburgerSVG, LogoSVG } from '../Icons'
 import Link from 'next/link'
 
 export const Navbar = () => {
+	const [menu, toggleMenu] = useState(false)
+
 	return (
 		<div className='flex justify-between items-center py-8 content-container'>
 			<LogoSVG></LogoSVG>
 
-			<div className='hidden md:flex justify-between w-1/2 xl:w-1/3 items-center uppercase '>
+			<div className='hidden md:flex justify-between w-1/2 xl:w-1/3 items-center uppercase'>
 				<Link href='/home'>Home</Link>
 				<Link href='/projects'>Portfolio</Link>
 				<Link href='/contact'>Contact Me</Link>
 			</div>
 
-			<div className='block md:hidden'>
+			<div className='block md:hidden' onClick={() => toggleMenu(!menu)}>
 				<HamburgerSVG></HamburgerSVG>
 			</div>
+
+			{menu && (
+				<>
+					<div className='menu md:hidden' onClick={() => toggleMenu(false)}>
+						<Link href='/home'>
+							<div className='menu-button link-text'>Home</div>
+						</Link>
+						<Link href='/portfolio'>
+							<div className='menu-button link-text'>Portfolio</div>
+						</Link>
+						<Link href='/contact'>
+							<div className='menu-button link-text'>Contact Me</div>
+						</Link>
+					</div>
+					{/* close overlay */}
+					<div
+						id='overlay'
+						className='fixed w-screen h-screen top-0 left-0 bg-black bg-opacity-25 z-30'
+						onClick={(e) => {
+							toggleMenu(false)
+						}}
+					></div>
+				</>
+			)}
 		</div>
 	)
 }
