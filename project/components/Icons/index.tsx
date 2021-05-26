23a969bd9c0f03f4475928a2e9a33ebe58700936
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 18:47:17 -0700

    add arrow icons component

--- project/components/Icons/index.tsx
+++ project/components/Icons/index.tsx
@@ -78,3 +78,19 @@ export const GithubSVG = ({
 		</svg>
 	)
 }
+
+export const ArrowLeftSVG = () => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' width='10' height='16'>
+			<path fill='none' stroke='#33323D' d='M9 0L1 8l8 8' />
+		</svg>
+	)
+}
+
+export const ArrowRightSVG = () => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' width='10' height='16'>
+			<path fill='none' stroke='#33323D' d='M1 0l8 8-8 8' />
+		</svg>
+	)
+}
