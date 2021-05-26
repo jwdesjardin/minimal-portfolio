deb3059a4438a5608cdf9bd2e044837d7fb4b8e9
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 18:46:30 -0700

    add navigation component

---
+++ project/components/Navigation/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ArrowLeftSVG, ArrowRightSVG } from '../Icons'
+
+const Navigation = () => {
+	return (
+		<div className='content-container section-border-y-grey flex flex-row'>
+			<div className='w-1/2 border-r-2 border-myGrey-300 pb-6 md:pb-8 md:flex md:items-center  md:flex-row'>
+				<div className='my-6 md:mr-8'>
+					<ArrowLeftSVG></ArrowLeftSVG>
+				</div>
+				<div className='md:mt-8'>
+					<h2 className='heading-text-thin-md'>Manage</h2>
+					<p className='medium-text text-myGrey-500'>Previous Project</p>
+				</div>
+			</div>
+			<div className='flex flex-col items-end ml-auto text-right pb-6 md:pb-8 md:flex md:items-center  md:flex-row-reverse'>
+				<div className='my-6 md:ml-8'>
+					<ArrowRightSVG></ArrowRightSVG>
+				</div>
+				<div className='md:mt-8'>
+					<h2 className='heading-text-thin-md'>Insure</h2>
+					<p className='medium-text text-myGrey-500'>Next Project</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Navigation
