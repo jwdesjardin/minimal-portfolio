53dc69e830cfa5135dc68ed6933fe9d590d3d22f
joey desjardins <jwdesjardin@gmail.com>
2021-05-25 18:52:53 -0700

    add container to navbar

--- project/components/Navbar/index.tsx
+++ project/components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export const Navbar = () => {
 	return (
-		<div className='flex justify-between items-center w-full p-8'>
+		<div className='flex justify-between items-center w-full p-8 xl:px-0 content-container'>
 			<LogoSVG></LogoSVG>
 
 			<div className='hidden md:flex justify-between w-64 items-center '>
